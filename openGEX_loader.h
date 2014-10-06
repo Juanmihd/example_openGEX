d1103739eec60e9dfa4b5ce963161a9cd52ac203
Juanmihd <juanmihd@gmail.com>
2014-10-06 13:34:07 +0100

    Adding tester if the file exist before loading

--- openGEX_loader.h
+++ openGEX_loader.h
@@ -11,12 +11,30 @@ namespace octet { namespace loaders {
     class openGEX_loader {
       /// Source for the image for reloads
       string url;
-
+      /// Document that has the file
+      TiXmlDocument doc;
     public:
+      /// Constructor of the openGEX_loader class
       openGEX_loader();
 
-      void loadFile(const char * name){
-        this->url = name;
+      /// Loads the file with the "name" name of file. First it test if it exists
+      bool loadFile(const char * name){
+        url = name;
+        url.truncate(url.filename_pos());
+        const char *path = app_utils::get_path(url);
+        char buf[256];
+        getcwd(buf, sizeof(buf));
+        doc.LoadFile(path);
+
+        TiXmlElement *top = doc.RootElement();
+
+        //Test if the file exists
+        if (!top){
+          printf("file %s not found\n", path);
+          return false;
+        }
+        //Test file
+        return true;
       }
     };
   }
