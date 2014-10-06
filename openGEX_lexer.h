589f7c0700a9c3159697271a03e230d9154d5525
Juanmihd <juanmihd@gmail.com>
2014-10-06 12:59:13 +0100

    Creating the first version of the lexer, able to ignore comments

---
+++ openGEX_lexer.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @file openGEX_lexer.h
+/// @author Juanmi Huertas Delgado
+/// @brief This is the lexer to load the files of OpenGEX
+///
+////////////////////////////////////////////////////////////////////////////////
+
+
+/// @brief This class is the openGEX lexer, it will read the array of characters and get tokes
+namespace octet
+{
+  namespace openGEX{
+    class openGEX_lexer {
+      /// url file
+      /// int8 pointer to the text
+
+    public:
+       openGEX_lexer();
+
+       void loadFile();
+    };
+  }
+}
