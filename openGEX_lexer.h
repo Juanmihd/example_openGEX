7f88532edcc198395520b5b9c044c862249541c5
Juanmihd <juanmihd@gmail.com>
2014-10-12 12:57:20 +0100

    Function process_structure moved from dll to gex

--- openGEX_lexer.h
+++ openGEX_lexer.h
@@ -23,6 +23,33 @@ namespace octet
         init();
       }
 
+      /// @brief  This function will process the currentChar to look for the next token and study it
+      /// openDDL is divided into structures
+      bool process_structure(){
+        string word;
+        //printf("%x ", currentChar[0]);
+        // first thing is to check if it's a comment
+        if (is_comment()) ignore_comment(); //if it's a comment analyze it (that means, ignore it)
+        else if (is_whiteSpace()); // check if it's a whitespace, do nothing (that's why there is a ;)
+        else{ // It's a real structure! But it can be IDENTIFIER or DATATYPE
+          printf("Structure Started!\n");
+          word = read_word();
+          printf("%s\n", word);
+          //read word
+          if (is_dataType()){
+            // process_structureData(); //As it's a Data type, now it can be single data list or data array list!
+          }
+          else if (is_identifier(word)){
+            // process_structureData(); //As it's a Identifier type, now check name? properties? and then { structure(s)? }
+            printf("\t----Is a identifier!!----\n");
+          }
+          else //if it's nothing of the above is an error
+            ;//assert(0 && "It's not a proper structure");
+          printf("Structure ended!\n\n");
+        }
+        return true;
+      }
+
       /// @brief This will be the function that creates de process of the lexer receiving as parameter the array of characters
       bool lexer_file(dynarray<uint8_t> file){
         sizeRead = 0;
