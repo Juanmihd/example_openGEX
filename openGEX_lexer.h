f225bfc0814748d9bc223797deb76a571ebdcc6f
Juanmihd <juanmihd@gmail.com>
2014-10-14 14:30:57 +0100

    Addind no_error correctly to lexerfile function

--- openGEX_lexer.h
+++ openGEX_lexer.h
@@ -163,17 +163,16 @@ namespace octet
 
       /// @brief This will be the function that creates de process of the lexer receiving as parameter the array of characters
       bool lexer_file(dynarray<uint8_t> file){
-        return no_error = true;
+        bool no_error = true;
         sizeRead = 0;
         buffer = file;
         currentChar = &buffer[0];
         bufferSize = buffer.size();
-        bool keep_on = true;
         // It's starting to process all the array of characters starting with the first
         // Will do this until the end of the file
-        while (!is_end_file() && keep_on){
+        while (!is_end_file() && no_error){
           //Process token (in openDDL is a structure) when you find it (when it's not a whiteSpace)
-          keep_on = process_structure();
+          no_error = process_structure();
           //get new token
           get_next_char();
         }
