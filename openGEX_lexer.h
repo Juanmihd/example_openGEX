ff86924bc4071addf0bba2af6d2db0a6b78f99cb
Juanmihd <juanmihd@gmail.com>
2014-10-29 08:52:35 +0000

    Adding new functions to process with openGEX information the structures. This is a first layer of functions, it has to be analyze into detail. Once that it's completely analyze (at the end of each top-level structure) it should store it into octet.

--- openGEX_lexer.h
+++ openGEX_lexer.h
@@ -32,6 +32,72 @@ namespace octet
         }
       }
 
+      ////////////////////////////////////////////////////////////////////////////////
+      /// @brief This will process with openGEX info a data type structure (previously analized by openDDL lexer)
+      /// @return True if everything went well, false if there was some problem
+      ////////////////////////////////////////////////////////////////////////////////
+      bool openGEX_data_type_structure(openDDL_data_type_structure * structure){
+        int tempID;
+        //Obtaining the data_type of the structure
+        printf("The type is: ");
+        tempID = (structure)->get_typeID();
+        if (tempID < 0)
+          printf("(((ERROR)))\n");
+        else{
+          printf("%s", ddl_token::token_name(tempID).c_str());
+          printf("\n");
+        }
+        //Check the name of the structure!
+        tempID = structure->get_nameID();
+        if (tempID >= 0){
+          printf("The name is ");
+          printf("%s", names_.get_key(tempID));
+          printf("\n");
+        }
+        return true;
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////
+      /// @brief This will process with openGEX info a identifier structure (previously analized by openDDL lexer)
+      /// @return True if everything went well, false if there was some problem
+      ////////////////////////////////////////////////////////////////////////////////
+      bool openGEX_identifier_structure(openDDL_identifier_structure * structure){
+        int tempID;
+        //Obtaining the identifier of the structure
+        printf("The identifier is: ");
+        tempID = (structure)->get_identifierID();
+        if (tempID < 0)
+          printf("(((ERROR)))\n");
+        else{
+          printf("%s", gex_ident::ident_name(tempID).c_str());
+          printf("\n");
+        }
+        //Check the name of the structure!
+        tempID = structure->get_nameID();
+        if (tempID >= 0){
+          printf("The name is ");
+          printf("%s", names_.get_key(tempID));
+          printf("\n");
+        }
+        return true;
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////
+      /// @brief This will check which type of structure to openGEX (previously analized by openDDL lexer)
+      /// @return True if everything went well, false if there was some problem
+      ////////////////////////////////////////////////////////////////////////////////
+      bool openGEX_structure(openDDL_structure * structure){
+        bool no_error = true;
+        //Check the type of the structure!
+        if (structure->get_type_structure() == 0){ //That means that it's a identifier structure!
+          no_error = openGEX_identifier_structure((openDDL_identifier_structure*) structure);
+        }
+        else{// That means that it's a data_type structure!!
+
+        }
+        return true;
+      }
+
     public:
       ////////////////////////////////////////////////////////////////////////////////
       /// @brief Constructor of lexer
@@ -42,33 +108,19 @@ namespace octet
       }
 
       ////////////////////////////////////////////////////////////////////////////////
-      /// @brief Constructor of lexer
+      /// @brief This function will analize all the data obtained by the openDDL lexer process
       /// @return True if everything went well, false if there was some problem
       ////////////////////////////////////////////////////////////////////////////////
       bool openGEX_data(){
         int numStructures = openDDL_file.size();
-        int tempID;
         openDDL_structure * bigStructure;
         printf("Starting to reading the file containin %i megaStructures:\n", numStructures);
         for (int i = 0; i < numStructures; ++i){
+          //Get next structure
           bigStructure = openDDL_file[i];
+          //Check the type of the structure and the identificator or data_type!
           printf("\n-- Structure %i:\n", i+1);
-          printf("Type: ");
-          printf((bigStructure->get_type_structure() == 0 ? "identificator\n" : "data_type\n"));
-          tempID = ((openDDL_identifier_structure*)bigStructure)->get_identifierID();
-          if (tempID < 0)
-            printf("(((ERROR)))\n");
-          else{
-            printf("The identifier is: ");
-            printf("%s", gex_ident::ident_name(tempID).c_str());
-            printf("\n");
-          }
-          tempID = bigStructure->get_nameID();
-          if (tempID >= 0){
-            printf("The name is ");
-            printf("%s", names_.get_key(tempID));
-            printf("\n");
-          }
+          openGEX_structure(bigStructure);
         }
         printf("\n");
         return true;
