3aa24a18b9d10f5176f62c00d18bec3a06617417
Juanmihd <juanmihd@gmail.com>
2014-10-29 14:49:12 +0000

    Doxygen documentation for the new function created!

--- openDDL_structures.h
+++ openDDL_structures.h
@@ -30,9 +30,14 @@ namespace octet{
     enum value_type_DDL   { UINT = 0, INT = 1, BOOL = 2, FLOAT = 3, STRING = 4, REF = 5, TYPE = 6};
 
 
+    ////////////////////////////////////////////////////////////////////////////////
+    /// @brief This is a handy function that converts from openDDL_token to this literal type
+    /// @param  token This is the token from 
+    ////////////////////////////////////////////////////////////////////////////////
     int convert_type_token_to_DDL(int token){
       return (token == 0 ? 2 : (token < 5 ? 1 : (token < 9 ? 0 : (token < 11 ? 3 : token - 7))));
     }
+
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief This struct is represent a data_literal
 ////////////////////////////////////////////////////////////////////////////////
